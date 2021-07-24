d1902f60fcef6fac2fb1a4b695e28eedac1f81c2
Arthur <deaquinodesign@gmail.com>
2021-07-24 10:17:52 -0300

    Aula 1 - Como fazer comentários

---
+++ aula-1-como-fazer-comentarios.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+  Aula 1 - Como fazer comentários
+ */
+
+int main() {
+  // Esse é um comentário de uma linha
+
+  /*
+     Esse é um bloco de cometário,
+     dentro do bloco podemos escreve
+     quantas linhas quisermos
+   */
+
+  printf("Aprendendo comentarios na linguagem C\n");
+  printf("Hello world\n");
+  return 0;
+}
